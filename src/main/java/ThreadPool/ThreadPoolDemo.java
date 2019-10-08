adb379770f3313225ad4a54bb7a00ca0303c80c0
杨丰畅 <ayang818@qq.com>
2019-10-08 20:10:57 +0800

    use CustomizeThreadPool to calcute FutureTask

--- src/main/java/ThreadPool/ThreadPoolDemo.java
+++ src/main/java/ThreadPool/ThreadPoolDemo.java
@@ -1,12 +1,76 @@
 package ThreadPool;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadPoolDemo {
-    public static void main(String[] args) {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(50, 100, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+    static class CustomizeThreadFactory implements ThreadFactory {
+        private final String poolName ;
 
+        public CustomizeThreadFactory(String name) {
+            this.poolName = name;
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            return new CustomizeThread(r);
+        }
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        Executors.newCachedThreadPool();
+//        Executors.newScheduledThreadPool(10);
+//        Executors.newFixedThreadPool(10);
+//        Executors.newSingleThreadExecutor();
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(50, 100,
+                60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new ThreadPoolDemo.CustomizeThreadFactory("calc-pool"),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+        Callable<Integer> task = () -> {
+            Integer ans = 0;
+            for (int i = 0; i <= 100; i++) {
+                ans += i;
+            }
+            return ans;
+        };
+        List<Future<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Future<Integer> submit = threadPoolExecutor.submit(task);
+            list.add(submit);
+        }
+
+        Integer ans = 0;
+        for (Future<Integer> integerFuture : list) {
+            ans += integerFuture.get();
+        }
+
+        System.out.println(ans);
+        threadPoolExecutor.shutdown();
+    }
+}
+
+class CustomizeThread extends  Thread {
+    private static final String DEFAULT_NAME = "CustomizeThread";
+    private static AtomicInteger created = new AtomicInteger();
+    private static AtomicInteger alived = new AtomicInteger();
+    private Runnable target;
+
+    CustomizeThread(Runnable runnable) {
+        super(runnable, DEFAULT_NAME + "-" + created.incrementAndGet());
+        target = runnable;
+        setUncaughtExceptionHandler((t, e) -> {
+
+        });
+    }
+
+    @Override
+    public void run() {
+        try  {
+            target.run();
+            alived.incrementAndGet();
+        } finally {
+            alived.decrementAndGet();
+        }
     }
 }
